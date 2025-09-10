eb0f2e2aa30d7c343d72bb3fad3a4ce7d885291d
wesleimarcelo67 <58061353+wesleimarcelo67@users.noreply.github.com>
2025-09-10 01:50:51 -0300

    change aws region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,12 +3,9 @@
 terraform {
   backend "s3" {
     # Use the unique bucket name you just created
-    bucket         = "linkshrink-terraform-state-cem-12345"
+    bucket         = "linkshrink-microservices-tfstate"
     # This is the path/filename for the state file inside the bucket
     key            = "global/terraform.tfstate"
-    region         = "eu-north-1"
-
-    # Use the lock table name you just created
-    dynamodb_table = "linkshrink-terraform-lock"
+    region         = "us-east-2"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "image_tag" {
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
-  default     = "eu-north-1"
+  default     = "us-east-2"
 }
 
 variable "link_db_password" {
